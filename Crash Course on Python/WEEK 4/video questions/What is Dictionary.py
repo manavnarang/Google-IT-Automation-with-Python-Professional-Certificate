b821f992642d6c410e7ff49411b473bf3cabfbd7
Manav Narang <36950613+manavnarang@users.noreply.github.com>
2020-05-26 18:08:46 +0530

    Update What is Dictionary.py

--- Crash Course on Python/WEEK 4/video questions/What is Dictionary.py
+++ Crash Course on Python/WEEK 4/video questions/What is Dictionary.py
@@ -1,1 +1,13 @@
+#The "toc" dictionary represents the table of contents for a book. 
+#Fill in the blanks to do the following: 
+#1) Add an entry for Epilogue on page 39. 
+#2) Change the page number for Chapter 3 to 24. 
+#3) Display the new dictionary contents. 
+#4) Display True if there is Chapter 5, False if there isn't.
 
+#Solution:
+toc = {"Introduction":1, "Chapter 1":4, "Chapter 2":11, "Chapter 3":25, "Chapter 4":30}
+toc["Epilogue"] = 39
+toc["Chapter 3"]=24
+print(toc)
+print('Chapter 5' in toc)
