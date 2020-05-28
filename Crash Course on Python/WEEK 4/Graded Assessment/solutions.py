83dc05d9fb25a4affa19c8362267bbac248fc3a3
Manav Narang <36950613+manavnarang@users.noreply.github.com>
2020-05-28 18:22:12 +0530

    Update solutions.py

--- Crash Course on Python/WEEK 4/Graded Assessment/solutions.py
+++ Crash Course on Python/WEEK 4/Graded Assessment/solutions.py
@@ -1,4 +1,105 @@
+#Q1:
+def format_address(address_string):
+  # Declare variables
+
+  # Separate the address string into parts
+  list1=address_string.split(" ")
+
+  # Traverse through the address parts
+  st=""
+  for x in range (len(list1)):
+    if(x!=0):
+      st+=list1[x]+" "
+    # Determine if the address part is the
+    # house number or part of the street name
+
+  # Does anything else need to be done 
+  # before returning the result?
+  
+  # Return the formatted string  
+  return "house number {} on street named {}".format(list1[0],st.strip())
+
+print(format_address("123 Main Street"))
+# Should print: "house number 123 on street named Main Street"
+
+print(format_address("1001 1st Ave"))
+# Should print: "house number 1001 on street named 1st Ave"
+
+print(format_address("55 North Center Drive"))
+# Should print "house number 55 on street named North Center Drive"
 
 
+#Q2:
+import re
+def highlight_word(sentence, word):
+	return sentence.replace(word,word.upper())
+
+print(highlight_word("Have a nice day", "nice"))
+print(highlight_word("Shhh, don't be so loud!", "loud"))
+print(highlight_word("Automating with Python is fun", "fun"))
 
 
+#Q3
+def combine_lists(list1, list2):
+  # Generate a new list containing the elements of list2
+  # Followed by the elements of list1 in reverse order
+  return list2+list1[::-1]
+  
+	
+Jamies_list = ["Alice", "Cindy", "Bobby", "Jan", "Peter"]
+Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
+
+print(combine_lists(Jamies_list, Drews_list))
+
+
+#Q4:
+def squares(start, end):
+	return [ x*x for x in range(start,end+1) ]
+
+print(squares(2, 3)) # Should be [4, 9]
+print(squares(1, 5)) # Should be [1, 4, 9, 16, 25]
+print(squares(0, 10)) # Should be [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+
+#Q5:
+def car_listing(car_prices):
+  result = ""
+  for key,value in car_prices.items():
+    result += ("{} costs {} dollars".format(key,value) + "\n")
+  return result
+
+print(car_listing({"Kia Soul":19000, "Lamborghini Diablo":55000, "Ford Fiesta":13000, "Toyota Prius":24000}))
+
+
+#Q6:
+def combine_guests(guests1, guests2):
+  # Combine both dictionaries into one, with each key listed 
+  # only once, and the value from guests1 taking precedence
+  guests2.update(guests1)
+  return guests2
+
+Rorys_guests = { "Adam":2, "Brenda":3, "David":1, "Jose":3, "Charlotte":2, "Terry":1, "Robert":4}
+Taylors_guests = { "David":4, "Nancy":1, "Robert":2, "Adam":1, "Samantha":3, "Chris":5}
+
+print(combine_guests(Rorys_guests, Taylors_guests))
+
+
+#Q7:
+def count_letters(text):
+    result = {}
+    text = text.lower()
+    for letter in text:
+        if letter.isalpha():
+            if letter in result.keys():
+                result[letter] += 1
+            else:
+                result[letter] = 1
+    return result
+print(count_letters("AaBbCc"))
+# Should be {'a': 2, 'b': 2, 'c': 2}
+
+print(count_letters("Math is fun! 2+2=4"))
+# Should be {'m': 1, 'a': 1, 't': 1, 'h': 1, 'i': 1, 's': 1, 'f': 1, 'u': 1, 'n': 1}
+
+print(count_letters("This is a sentence."))
+# Should be {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}
